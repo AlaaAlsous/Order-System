9f3518e944190b65a7b9e6f4757dff5f7b0b2832
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-04 10:21:38 +0100

    Add Save method to insert OrderItem into database

--- OrderSystem/OrderItem.cs
+++ OrderSystem/OrderItem.cs
@@ -10,5 +10,22 @@ namespace OrderSystem
         public string Description { get; set; } = "";
         public int Quantity { get; set; }
         public decimal Price { get; set; }
+
+        public void Save(SqliteConnection conn)
+        {
+            using var command = conn.CreateCommand();
+            command.CommandText = @"
+            INSERT INTO order_rows (order_id, product_id, description, quantity, unit_price)
+            VALUES (@order_id, @product_id, @description, @quantity, @unit_price)
+            RETURNING Id;
+            ";
+            command.Parameters.AddWithValue("@order_id", OrderId);
+            command.Parameters.AddWithValue("@product_id", ProductId);
+            command.Parameters.AddWithValue("@description", Description);
+            command.Parameters.AddWithValue("@quantity", Quantity);
+            command.Parameters.AddWithValue("@unit_price", Price);
+
+            Id = Convert.ToInt32(command.ExecuteScalar());
+        }
     }
 }
