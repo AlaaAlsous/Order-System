ca8a16cf31f5bdf4230d79ef359c0e38cd714b2f
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-04 10:08:35 +0100

    Add input validation for Customer name and email

--- OrderSystem/Customer.cs
+++ OrderSystem/Customer.cs
@@ -25,6 +25,40 @@ namespace OrderSystem
 
             Id = Convert.ToInt32(command.ExecuteScalar());
         }
+
+                public static void Add(SqliteConnection conn)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            ConsoleHelper.TextColor(ConsoleHelper.CenterText("═══════════════════════════════════════", Console.WindowWidth - 1), ConsoleColor.DarkCyan);
+            ConsoleHelper.TextColor(ConsoleHelper.CenterText("CREATE CUSTOMER", Console.WindowWidth - 1), ConsoleColor.Cyan);
+            ConsoleHelper.TextColor(ConsoleHelper.CenterText("═══════════════════════════════════════", Console.WindowWidth - 1), ConsoleColor.DarkCyan);
+            Console.WriteLine();
+
+            Customer customer = new Customer();
+
+            while (true)
+            {
+                Console.Write("Name: ");
+                var input = Console.ReadLine()?.Trim() ?? "";
+                if (input.Length >= 3 && input.Length <= 20)
+                {
+                    customer.Name = input; break;
+                }
+                ConsoleHelper.TextColor("⚠️ Name cannot be empty. You must provide a valid name (between 3 and 20 characters).\n", ConsoleColor.Red
+            );
+            }
+            while (true)
+            {
+                Console.Write("Email: ");
+                var input = Console.ReadLine()?.Trim() ?? "";
+                if (!string.IsNullOrWhiteSpace(input) && input.Contains("@") && input.Contains("."))
+                {
+                    customer.Email = input; break;
+                }
+                ConsoleHelper.TextColor("⚠️ Email cannot be empty. You must provide a valid email.", ConsoleColor.Red);
+            }
+        }
     }
 }
 
