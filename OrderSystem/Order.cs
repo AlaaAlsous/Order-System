1db67152e97deda567644fde54bff8dcf6a693c9
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-04 10:19:01 +0100

    Add Save method to insert Order into database

--- OrderSystem/Order.cs
+++ OrderSystem/Order.cs
@@ -8,5 +8,20 @@ namespace OrderSystem
         public int CustomerId { get; set; }
         public DateTime OrderDate { get; set; } = DateTime.UtcNow;
         public string Status { get; set; } = "Created";
+        public void Save(SqliteConnection conn)
+        {
+            using var command = conn.CreateCommand();
+            long unixTime = ((DateTimeOffset)OrderDate).ToUnixTimeSeconds();
+            command.CommandText = @"
+            INSERT INTO orders (customer_id, order_date, status)
+            VALUES (@customer_id, @order_date, @status)
+            RETURNING Id;
+            ";
+            command.Parameters.AddWithValue("@customer_id", CustomerId);
+            command.Parameters.AddWithValue("@order_date", unixTime);
+            command.Parameters.AddWithValue("@status", Status);
+
+            Id = Convert.ToInt32(command.ExecuteScalar());
+        }
     }
 }
