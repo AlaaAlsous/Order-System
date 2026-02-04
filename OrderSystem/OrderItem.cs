58eba09d6b685793d999bf422eb894f650c3a0eb
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-04 10:21:05 +0100

    Add OrderItem class

---
+++ OrderSystem/OrderItem.cs
@@ -0,0 +1,14 @@
+using Microsoft.Data.Sqlite;
+
+namespace OrderSystem
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string Description { get; set; } = "";
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
