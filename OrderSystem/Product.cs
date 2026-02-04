2fb74931f05b62ffeb10b75efabd41302d803a2a
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-04 10:14:34 +0100

    Add Save method to insert Product into database

--- OrderSystem/Product.cs
+++ OrderSystem/Product.cs
@@ -8,5 +8,19 @@ namespace OrderSystem
         public string Name { get; set; } = "";
         public double UnitPrice { get; set; }
         public int Stock { get; set; }
+        public void Save(SqliteConnection conn)
+        {
+            using var command = conn.CreateCommand();
+            command.CommandText = @"
+            INSERT INTO products (name, unit_price, stock)
+            VALUES (@name, @unit_price, @stock)
+            RETURNING Id;
+            ";
+            command.Parameters.AddWithValue("@name", Name);
+            command.Parameters.AddWithValue("@unit_price", UnitPrice);
+            command.Parameters.AddWithValue("@stock", Stock);
+
+            Id = Convert.ToInt32(command.ExecuteScalar());
+        }
     }
 }
