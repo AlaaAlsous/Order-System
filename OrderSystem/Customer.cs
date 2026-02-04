4c73085450323318c62a2446408bc7690106eaea
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-04 10:06:28 +0100

    Add Save method to insert Customer into database

--- OrderSystem/Customer.cs
+++ OrderSystem/Customer.cs
@@ -9,6 +9,22 @@ namespace OrderSystem
         public string Email { get; set; } = "";
         public string Phone { get; set; } = "";
         public string Address { get; set; } = "";
+
+        public void Save(SqliteConnection conn)
+        {
+            using var command = conn.CreateCommand();
+            command.CommandText = @"
+             INSERT INTO customers (name, email, phone, address)
+             VALUES (@name, @email, @phone, @address)
+             RETURNING Id;
+         ";
+            command.Parameters.AddWithValue("@name", Name);
+            command.Parameters.AddWithValue("@email", Email);
+            command.Parameters.AddWithValue("@phone", Phone);
+            command.Parameters.AddWithValue("@address", Address);
+
+            Id = Convert.ToInt32(command.ExecuteScalar());
+        }
     }
 }
 
