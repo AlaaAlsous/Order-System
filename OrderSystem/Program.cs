7e1986d484beb2dca6ff62b357033420412ae7e4
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-04 09:36:53 +0100

    Initialize SQLite connection

--- OrderSystem/Program.cs
+++ OrderSystem/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Microsoft.Data.Sqlite;
+
+namespace OrderSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var connection = new SqliteConnection("Data Source = order_system.sqlite");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.ExecuteNonQuery();
+        }
+    }
+}
